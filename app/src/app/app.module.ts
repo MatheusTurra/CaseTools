2829670b6214dd3c363752129e9b94b73bb40de5
MatheusTurra <matheusfr.318@gmail.com>
2021-04-01 22:47:19 -0300

    Main component created - Component generated by angular deleted

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MainComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainComponent] // START THE PAGE
 })
 export class AppModule { }
