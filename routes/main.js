abc0584d18c58a32c1246b76879a963e47253acd
MatheusTurra <matheusfr.318@gmail.com>
2021-04-09 01:01:40 -0300

    Route post added

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,11 @@ module.exports = (app) => {
     app.get("/", (req, res) => {
         res.json({teste: "foi"});
     });
+
+    app.post("/", (req, res) => {
+        res.send(req.body)
+        console.log(req.body);
+    });
     
     return app;
 }
